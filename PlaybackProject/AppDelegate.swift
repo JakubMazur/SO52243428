d90be2cf8347d2c5c30b926622c77663d15b68bc
Jakub Mazur <jakub.mazur@wingu.de>
2018-09-14 09:44:33 +0200

    add example

--- PlaybackProject/AppDelegate.swift
+++ PlaybackProject/AppDelegate.swift
@@ -7,40 +7,81 @@
 //
 
 import UIKit
+import MediaPlayer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    func playSth() {
+        self.setupNowPlaying()
+        self.setupRemoteTransportControls()
+    }
+    
+    var audio: AVAudioPlayer?
+    var session: AVAudioSession? = AVAudioSession.sharedInstance()
+    
+    func setupNowPlaying() {
+        do {
+            try session?.setCategory(AVAudioSessionCategoryPlayback)
+            try session?.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print(error)
+        }
+        do {
+            let url = Bundle.main.url(forResource: "example", withExtension: "mp3") // http://www.largesound.com/ashborytour/sound/
+            audio = try AVAudioPlayer(contentsOf: url!)
+        } catch {
+            print(error)
+        }
+        audio?.play()
+        
+        var nowPlayingInfo = [String : Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "My Track"
+        
+        if let image = UIImage(named: "image") {
+            nowPlayingInfo[MPMediaItemPropertyArtwork] =
+                MPMediaItemArtwork(boundsSize: image.size) { size in
+                    return image
+            }
+        }
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = audio!.currentTime
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = audio!.duration
+        nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = audio!.rate
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+    }
+    
+    func setupRemoteTransportControls() {
+        let commandCenter = MPRemoteCommandCenter.shared()
+        commandCenter.playCommand.addTarget { [unowned self] event in
+            if self.audio!.rate == 0.0 {
+                self.audio!.play()
+                return .success
+            }
+            return .commandFailed
+        }
+        
+        commandCenter.pauseCommand.addTarget { [unowned self] event in
+            if self.audio!.rate == 1.0 {
+                self.audio!.pause()
+                return .success
+            }
+            return .commandFailed
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.playSth()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10, execute: {
+                self.audio?.stop()
+                try? self.session?.setActive(false, with: .notifyOthersOnDeactivation)
+                MPNowPlayingInfoCenter.default().nowPlayingInfo = [:]
+            })
+        }
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
